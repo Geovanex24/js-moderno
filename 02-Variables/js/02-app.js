ef256b00f3439c3ecaa6020702a3b62bca5ec3f5
geovanex24 <geovanex24@hotmail.com>
2023-09-15 21:43:32 -0600

    Variables con let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,23 @@
+// -- Variables: let --
+
+let producto = "Tablet";
+
+// Permite re-asignar el valor
+producto = "Monitor";
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+// Se pueden iniciarlizar sin valor y asginarlo después
+let precio;
+
+precio = 300;
+
+console.log(precio);
+
+// La diferencia con respecto a var es que puede cambiar su valor (re-asignar), pero NO Re-declararse
+// let carro = "Toyota";
+// let carro = true;
