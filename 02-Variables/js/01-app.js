f43a6c8f4cdcfc48d7bdf51094a097fba30b9136
geovanex24 <geovanex24@hotmail.com>
2023-09-15 21:31:10 -0600

    Variables con var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,37 @@
+// -- Variables: var --
+
+// Se declara e inicializa con un valor
+var producto = "Monitor de 24 pulgadas";
+
+// Se pueden re-asignar o modificar su valor
+producto = "Monitor de 19 pulgadas";
+
+/** Es un lenguaje dinámico y no tipado, por lo que no hay que indicar el tipo de dato en la variable **/
+producto = 20;
+console.log(producto);
+
+// Se pueden iniciarlizar sin valor y asginarlo después
+var disponible;
+
+disponible = true;
+
+disponible = false;
+
+// Inicializar múltiples variables
+var categoria = "Computadoras",
+  marca = "Marca Famosa",
+  calificación = 5;
+
+// REGLAS
+/* 
+  - Las variables pueden tener letras, números, ciertos carácteres
+  - PERO NO pueden comenzar con "números" 
+  - Pueden iniciar con "-"
+*/
+
+// -- ESTILOS PARA CREAR VARIABLES --
+
+var nombre_producto = "Monitor HD"; // snacke_case
+var nombreProducto = "Monitor HD"; //camelCase
+var NombreProducto = "Monitor HD"; //PascalCase: poco usado en JS, más que cuando se crear una clase
+var nombreproducto = "Monitor HD";
