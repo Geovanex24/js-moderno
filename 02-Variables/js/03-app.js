920a25472c727c609b84fd03cdaf5ffff2c45790
geovanex24 <geovanex24@hotmail.com>
2023-09-15 22:38:37 -0600

    Variables con const

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,18 @@
+// -- Variables: const --
+
+/**
+ * DIFERENCIAS ENTRE LET y CONST
+ */
+
+const producto = "Monitor";
+
+// 1. Const no se pueden re-asignar
+producto = "Tablet";
+
+// 2. Deben inicializarse con un valor
+
+const precio = 20;
+
+console.log(precio);
+
+// Consejo: Si el valor va a cambiar o aún no se sabe, utiliza let. Si por el contrario utiliza const.
